de3c5e3d1c295ecdc7360d305ca44c08676d9ae1
wagnermalves <wagnermalves@gmail.com>
2021-10-13 20:10:45 -0300

    Removida pasta node_modules

--- node_modules/node-opcua-hostname/test/test_hostname.js
+++
@@ -1,8 +0,0 @@
-const get_fully_qualified_domain_name = require("..").get_fully_qualified_domain_name;
-// tslint:disable:no-console
-describe("hostname", () => {
-    it("It should retrieve the fully qualified domain name", () => {
-        const fqdn = get_fully_qualified_domain_name();
-        console.log("fqdn =", fqdn);
-    });
-});
